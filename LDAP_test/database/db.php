1b240dee4a94f4548186a6145d0b89c61f7dc1d3
Marc-AntoineMarie <mmarie.galaxie@gmail.com>
2025-06-03 16:33:40 +0200

    test

--- LDAP_test/database/db.php
+++ LDAP_test/database/db.php
@@ -1,21 +1,26 @@
 <?php
 try {
-    // Établir la connexion avec la base de données
+    // Paramètres de connexion
     $host = 'localhost';
     $db = 'LDAP';
     $user = 'root';
     $pass = 'Macacagne05';
     $charset = 'utf8mb4';
+
     $options = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false,
     ];
-    $pdo = new PDO('mysql:host=' . $host . '; dbname=' . $db, $user, $pass);
-    echo "db.php-> tout fonctionne batard | ";
+
+    // Connexion PDO
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $pdo = new PDO($dsn, $user, $pass, $options);
+
+    echo "db.php-> tout fonctionne | ";
+
 } catch (PDOException $e) {
     echo "Erreur de connexion : " . $e->getMessage();
     exit;
 }
-
 ?>
