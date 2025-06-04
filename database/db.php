43dc568c5fd428f0b87fa76aa5453dfb97e6b756
Marc-AntoineMarie <mmarie.galaxie@gmail.com>
2025-06-04 11:12:51 +0200

    db fake

--- database/db.php
+++ database/db.php
@@ -2,15 +2,15 @@
 try {
     // Établir la connexion avec la base de données
     $host = 'localhost';
-    $db   = 'LDAP';
+    $db = 'LDAP';
     $user = 'gcadmin';
-    $pass = 'Y7x49spyK5MF';
+    $pass = 'ton_mdp';
     $charset = 'utf8mb4';
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
     $options = [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false,
+        PDO::ATTR_EMULATE_PREPARES => false,
     ];
     $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (PDOException $e) {
